9265db068e93a32c1b830eaf4d67f1d6232400f9
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 18:48:02 -0300

    Vesion: 000.001.000.000

---
+++ src/Commands/Concerns/InteractsWithServers.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Laravel\Octane\Commands\Concerns;
+
+use Laravel\Octane\Exceptions\ServerShutdownException;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
+
+trait InteractsWithServers
+{
+    /**
+     * The callable used to stop the server, if any.
+     *
+     * @var \Closure|null
+     */
+    protected $stopServerUsing;
+
+    /**
+     * Run the given server process.
+     *
+     * @param  \Symfony\Component\Process\Process  $server
+     * @param  \Laravel\Octane\Swoole\ServerProcessInspector|\Laravel\Octane\RoadRunner\ServerProcessInspector  $inspector
+     * @param  string  $type
+     * @return int
+     */
+    protected function runServer($server, $inspector, $type)
+    {
+        while (! $server->isStarted()) {
+            sleep(1);
+        }
+
+        $this->writeServerRunningMessage();
+
+        $watcher = $this->startServerWatcher();
+
+        $this->stopServerUsing = function () use ($type, $watcher) {
+            $watcher->stop();
+
+            $this->callSilent('octane:stop', [
+                '--server' => $type,
+            ]);
+
+            $this->stopServerUsing = null;
+        };
+
+        try {
+            while ($server->isRunning()) {
+                $this->writeServerOutput($server);
+
+                if ($watcher->isRunning() &&
+                    $watcher->getIncrementalOutput()) {
+                    $this->info('Application change detected. Restarting workers…');
+
+                    $inspector->reloadServer();
+                }
+
+                usleep(500 * 1000);
+            }
+
+            $this->writeServerOutput($server);
+        } catch (ServerShutdownException $e) {
+            return 1;
+        } finally {
+            $this->stopServer();
+        }
+
+        return $server->getExitCode();
+    }
+
+    /**
+     * Start the watcher process for the server.
+     *
+     * @return \Symfony\Component\Process\Process|object
+     */
+    protected function startServerWatcher()
+    {
+        if (! $this->option('watch')) {
+            return new class {
+                public function __call($method, $parameters)
+                {
+                    return null;
+                }
+            };
+        }
+
+        return tap(new Process([
+            (new ExecutableFinder)->find('node'), 'file-watcher.js', base_path(),
+        ], realpath(__DIR__.'/../../../bin'), null, null, null))->start();
+    }
+
+    /**
+     * Stop the server.
+     *
+     * @return void
+     */
+    protected function stopServer()
+    {
+        if ($this->stopServerUsing) {
+            $this->stopServerUsing->__invoke();
+        }
+    }
+
+    /**
+     * Write the server start "message" to the console.
+     *
+     * @return void
+     */
+    protected function writeServerRunningMessage()
+    {
+        $this->info('Server running…');
+
+        $this->output->writeln([
+            '',
+            '  Local: <fg=white;options=bold>http://'.$this->option('host').':'.$this->option('port').' </>',
+            '',
+            '  <fg=yellow>Use Ctrl+C to stop the server</>',
+            '',
+        ]);
+    }
+
+    /**
+     * Returns the list of signals to subscribe.
+     *
+     * @return array
+     */
+    public function getSubscribedSignals(): array
+    {
+        return [SIGINT, SIGTERM];
+    }
+
+    /**
+     * The method will be called when the application is signaled.
+     *
+     * @param int $signal
+     */
+    public function handleSignal(int $signal): void
+    {
+        $this->stopServer();
+    }
+}
---
+++ src/Commands/StartRoadRunnerCommand.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Laravel\Octane\Commands;
+
+use Illuminate\Support\Str;
+use Laravel\Octane\RoadRunner\ServerProcessInspector;
+use Laravel\Octane\RoadRunner\ServerStateFile;
+use Symfony\Component\Console\Command\SignalableCommandInterface;
+use Symfony\Component\Process\Process;
+
+class StartRoadRunnerCommand extends Command implements SignalableCommandInterface
+{
+    use Concerns\InstallsRoadRunnerDependencies, Concerns\InteractsWithServers;
+
+    /**
+     * The command's signature.
+     *
+     * @var string
+     */
+    public $signature = 'octane:roadrunner
+                    {--host=127.0.0.1 : The IP address the server should bind to}
+                    {--port=8000 : The port the server should be available on}
+                    {--workers=auto : The number of workers that should be available to handle requests}
+                    {--max-requests=500 : The number of requests to process before reloading the server}
+                    {--watch : Automatically reload the server when the application is modified}';
+
+    /**
+     * The command's description.
+     *
+     * @var string
+     */
+    public $description = 'Start the Octane RoadRunner server';
+
+    /**
+     * Indicates whether the command should be shown in the Artisan command list.
+     *
+     * @var bool
+     */
+    protected $hidden = true;
+
+    /**
+     * Handle the command.
+     *
+     * @param  \Laravel\Octane\RoadRunner\ServerProcessInspector  $inspector
+     * @param  \Laravel\Octane\RoadRunner\ServerStateFile  $serverStateFile
+     * @return int
+     */
+    public function handle(ServerProcessInspector $inspector, ServerStateFile $serverStateFile)
+    {
+        if (! $this->ensureRoadRunnerPackageIsInstalled()) {
+            return 1;
+        }
+
+        $roadRunnerBinary = $this->ensureRoadRunnerBinaryIsInstalled();
+
+        if ($inspector->serverIsRunning()) {
+            $this->error('RoadRunner server is already running.');
+
+            return 1;
+        }
+
+        $this->writeServerStateFile($serverStateFile);
+
+        touch(base_path('.rr.yaml'));
+
+        $server = tap(new Process(array_filter([
+            $roadRunnerBinary,
+            '-o', 'http.address='.$this->option('host').':'.$this->option('port'),
+            '-o', 'server.command=php ./vendor/bin/roadrunner-worker',
+            '-o', 'http.pool.num_workers='.$this->workerCount(),
+            '-o', 'http.pool.max_jobs='.$this->option('max-requests'),
+            '-o', 'http.pool.supervisor.exec_ttl='.$this->maxExecutionTime(),
+            '-o', 'http.static.dir=public',
+            '-o', 'http.middleware=static',
+            '-o', app()->environment('local') ? 'logs.mode=production' : 'logs.mode=none',
+            '-o', app()->environment('local') ? 'logs.level=debug' : 'logs.level=warning',
+            '-o', 'logs.output=stdout',
+            '-o', 'logs.encoding=json',
+            'serve',
+        ]), base_path(), ['APP_BASE_PATH' => base_path(), 'LARAVEL_OCTANE' => 1], null, null))->start();
+
+        $serverStateFile->writeProcessId($server->getPid());
+
+        return $this->runServer($server, $inspector, 'roadrunner');
+    }
+
+    /**
+     * Write the RoadRunner server state file.
+     *
+     * @param  \Laravel\Octane\RoadRunner\ServerStateFile  $serverStateFile
+     * @return void
+     */
+    protected function writeServerStateFile(
+        ServerStateFile $serverStateFile
+    ) {
+        $serverStateFile->writeState([
+            'appName' => config('app.name', 'Laravel'),
+            'host' => $this->option('host'),
+            'port' => $this->option('port'),
+            'workers' => $this->workerCount(),
+            'maxRequests' => $this->option('max-requests'),
+            'octaneConfig' => config('octane'),
+        ]);
+    }
+
+    /**
+     * Get the number of workers that should be started.
+     *
+     * @return int
+     */
+    protected function workerCount()
+    {
+        return $this->option('workers') == 'auto'
+                            ? 0
+                            : $this->option('workers', 0);
+    }
+
+    /**
+     * Get the maximum number of seconds that workers should be allowed to execute a single request.
+     *
+     * @return string
+     */
+    protected function maxExecutionTime()
+    {
+        return config('octane.max_execution_time', '30').'s';
+    }
+
+    /**
+     * Write the server process output to the console.
+     *
+     * @param  \Symfony\Component\Process\Process  $server
+     * @return void
+     */
+    protected function writeServerOutput($server)
+    {
+        Str::of($server->getIncrementalOutput())
+            ->explode("\n")
+            ->filter()
+            ->each(function ($output) {
+                if (! is_array($debug = json_decode($output, true))) {
+                    return $this->info($output);
+                }
+
+                if (is_array($stream = json_decode($debug['msg'], true))) {
+                    return $this->handleStream($stream);
+                }
+
+                if ($debug['level'] == 'debug' && isset($debug['remote'])) {
+                    [$statusCode, $method, $url] = explode(' ', $debug['msg']);
+
+                    $elapsed = Str::endsWith($debug['elapsed'], 'ms')
+                        ? substr($debug['elapsed'], 0, -2)
+                        : substr($debug['elapsed'], 0, -1) * 1000;
+
+                    return $this->requestInfo([
+                        'method' => $method,
+                        'url' => $url,
+                        'statusCode' => $statusCode,
+                        'duration' => (float) $elapsed,
+                    ]);
+                }
+            });
+
+        Str::of($server->getIncrementalErrorOutput())
+            ->explode("\n")
+            ->filter()
+            ->each(function ($output) {
+                if (! Str::contains($output, ['DEBUG', 'INFO', 'WARN'])) {
+                    $this->error($output);
+                }
+            });
+    }
+}
